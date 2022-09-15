d4eb53a89899d73554375eb63ad34178793b76ce
Poteryshka <zherebec_artem@mail.ru>
2022-09-15 21:39:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnTechDoc = document.getElementById('techDoc')
+const btnMainInfo = document.getElementById('mainInfo')
+
+let 
+
+btn.addEventListener('click', function(){ // отловить событие, можно добавить сколько угодно прослушек на одно и тоже событие
+    //     console.log('Click1');
+    //     image.remove(); // полностью удалить элемент из разметки
+    // })
+}
