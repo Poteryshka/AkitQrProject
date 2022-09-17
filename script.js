0e78c37fcd3bb7f0e2d9b1c4254c8c8778e82e13
Poteryshka <zherebec_artem@mail.ru>
2022-09-17 22:01:49 +0300

    Создал 5 страниц и главную менюшку (без дизайна чисто для перехода по страницам)

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const btnTechDoc = document.getElementById('techDoc')
 const btnMainInfo = document.getElementById('mainInfo')
+const btnDownload = document.getElementById('download')
+const btnAsk = document.getElementById('ask')
 
-let 
+let pageName = ['container-tech-doc', 'container-main-info', 'container-download', 'container-ask']
 
-btn.addEventListener('click', function(){ // отловить событие, можно добавить сколько угодно прослушек на одно и тоже событие
-    //     console.log('Click1');
-    //     image.remove(); // полностью удалить элемент из разметки
-    // })
+let switchPage = function(name){
+    console.log(name)
+    let activePage = document.querySelector('.' + name)
+    activePage.classList.remove('none')
+    for(item of pageName){
+        if(item != name){
+            let passivePage = document.querySelector('.' + item)
+            passivePage.classList.add('none')
+        }
+    }
 }
+
+btnMainInfo.addEventListener('click', () => {
+    switchPage('container-main-info')
+})
+
+btnTechDoc.addEventListener('click', () => {
+    switchPage('container-tech-doc')
+})
+
+btnDownload.addEventListener('click', () => {
+    switchPage('container-download')
+})
+
+btnAsk.addEventListener('click', () => {
+    switchPage('container-ask')
+})
+
+
